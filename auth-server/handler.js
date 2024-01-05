6c2ba18d7ea5f8bdf7f888e0a9dd948309772447
nadyathompson <127708727+nadyathompson@users.noreply.github.com>
2024-01-04 23:09:06 -0500

    4.3 Writing and Testing Serverless Functions

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -2,11 +2,11 @@
 
 const { google } = require("googleapis");
 const calendar = google.calendar("v3");
-const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
-const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = [
-  "https://nadyathompson.github.io/meet"
+const SCOPES = [
+  "https://www.googleapis.com/auth/calendar.events.public.readonly",
 ];
+const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
+const redirect_uris = ["https://nadyathompson.github.io/meet"];
 
 const oAuth2Client = new google.auth.OAuth2(
   CLIENT_ID,
@@ -17,7 +17,7 @@ const oAuth2Client = new google.auth.OAuth2(
 module.exports.getAuthURL = async () => {
   /**
    *
-   * Scopes array is passed to the `scope` option. 
+   * Scopes array is passed to the `scope` option.
    *
    */
   const authUrl = oAuth2Client.generateAuthUrl({
@@ -28,8 +28,8 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
     },
     body: JSON.stringify({
       authUrl,
@@ -37,7 +37,7 @@ module.exports.getAuthURL = async () => {
   };
 };
 
-/*module.exports.getAccessToken = async (event) => {
+module.exports.getAccessToken = async (event) => {
   // Decode authorization code extracted from the URL query
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
@@ -47,7 +47,7 @@ module.exports.getAuthURL = async () => {
      *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
      */
 
-    /*oAuth2Client.getToken(code, (error, response) => {
+    oAuth2Client.getToken(code, (error, response) => {
       if (error) {
         return reject(error);
       }
@@ -55,12 +55,12 @@ module.exports.getAuthURL = async () => {
     });
   })
     .then((results) => {
-      // Respond with OAuth token 
+      // Respond with OAuth token
       return {
         statusCode: 200,
         headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credentials': true,
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
         },
         body: JSON.stringify(results),
       };
@@ -72,4 +72,52 @@ module.exports.getAuthURL = async () => {
         body: JSON.stringify(error),
       };
     });
-};*/
+};
+
+module.exports.getCalendarEvents = async (event) => {
+  // Decode authorization code extracted from the URL query
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject) => {
+    /**
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “error” and “response”
+     */
+    calendar.events.list(
+      {
+        calendarId: CALENDAR_ID,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          return reject(error);
+        }
+        return resolve(response);
+      }
+    );
+  })
+    .then((results) => {
+      // Respond with OAuth token
+      return {
+        statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(results),
+      };
+    })
+    .catch((error) => {
+      // Handle error
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
+};
